b265fdbb4a72082d4f12beaa63008f4155fa80ab
ujwalshetty7022 <ujwalshetty172@gmail.com>
2024-02-17 23:35:55 +0530

    commit1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:libx/user_login.dart';
+import 'package:libx/userhome.dart';
 import 'user_signup.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: user_signup(),
+      home: userhome(),
     );
   }
 }
--- lib/userhome.dart
+++ lib/userhome.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class userhome extends StatefulWidget {
-  const userhome({super.key});
-
+  const userhome({Key? key});
   @override
   State<userhome> createState() => _userhomeState();
 }
@@ -12,7 +10,85 @@ class _userhomeState extends State<userhome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Hello world"),
+      //bottom navigation  bar
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.3), // Shadow color with opacity
+              spreadRadius: 1, // Spread radius
+              blurRadius: 4, // Blur radius
+              offset: Offset(0, -2), // Offset of the shadow (0 is for no offset in x-axis, -2 is for moving the shadow upwards)
+            ),
+          ],
+        ),
+        height: 118,
+        child: BottomNavigationBar(
+          backgroundColor: Colors.deepPurple,
+          items: [
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: const EdgeInsets.only(top: 25),
+                child: Icon(Icons.home),
+              ),
+              label: 'Home',
+              backgroundColor: Colors.deepPurple,
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: const EdgeInsets.only(top: 25),
+                child: Icon(Icons.book_rounded,),
+              ),
+              label: 'MyBooks',
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: const EdgeInsets.only(top: 25),
+                child: Icon(Icons.category),
+              ),
+              label: 'Category',
+            ),
+          ],
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.white,
+          selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+          unselectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+
+
+
+
+      //App Top navigation bar
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: AppBar(
+          backgroundColor: Colors.deepPurple,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: Text(
+                  'Logo',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 190),
+                  child: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
