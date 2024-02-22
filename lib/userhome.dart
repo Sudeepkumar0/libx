f976b50d2af71b77b3f88b53e67458e0073e431a
ujwalshetty7022 <ujwalshetty172@gmail.com>
2024-02-22 21:06:44 +0530

    carousel

--- lib/userhome.dart
+++ lib/userhome.dart
@@ -2,15 +2,58 @@ import 'package:flutter/material.dart';
 import 'Category.dart';
 import 'MyBooks.dart';
 import 'Profile.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-// Define a placeholder HomeScreen widget
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Column(
+        children: [
+          CarouselSlider(
+            items: [
+              Image.asset('assets/carousle_img 1.jpg'),
+              Image.asset('assets/carousle_img 2.jpg'),
+              Image.asset('assets/carousle_img 3.jpg'),
+            ],
+            options: CarouselOptions(
+              autoPlay: true,
+              //enlargeCenterPage: true,
+              //enableInfiniteScroll: true,
+              //aspectRatio: 16 / 9,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+            ),
+          ),
+          SizedBox(height: 5),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              for (int i = 0; i < 3; i++)
+                Container(
+                  width: 10,
+                  height: 10,
+                  margin: EdgeInsets.symmetric(horizontal: 2),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _currentIndex == i ? Colors.blue : Colors.grey,
+                  ),
+                ),
+            ],
+          ),
+        ],
+      ),
     );
-
   }
 }
 
@@ -44,34 +87,25 @@ class _UserHomeState extends State<UserHome> {
         child: Center(
           child: ListTile(
             title: Text('My title'),
-
           ),
         ),
-
       ),
-
-
       appBar: AppBar(
-        iconTheme:IconThemeData(color: Colors.white) ,
-        backgroundColor:Colors.deepPurple,
+        iconTheme: IconThemeData(color: Colors.white),
+        backgroundColor: Colors.deepPurple,
         leading: Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Expanded(
               child: Container(
-                width: 200,
-                height: 200,
+                width: 500,
+                height: 500,
                 child: Image.asset('assets/logo.png'),
-
               ),
             ),
           ],
         ),
       ),
-
-
-      //Bottom navigation
-
       bottomNavigationBar: NavigationBarTheme(
         data: NavigationBarThemeData(
           indicatorColor: Colors.deepPurple,
@@ -93,12 +127,14 @@ class _UserHomeState extends State<UserHome> {
             ),
             NavigationDestination(
               icon: Icon(Icons.category, size: 30),
-              selectedIcon: Icon(Icons.category, size: 30, color: Colors.white),
+              selectedIcon:
+              Icon(Icons.category, size: 30, color: Colors.white),
               label: 'Category',
             ),
             NavigationDestination(
               icon: Icon(Icons.book_rounded, size: 30),
-              selectedIcon: Icon(Icons.book_rounded, size: 30, color: Colors.white),
+              selectedIcon:
+              Icon(Icons.book_rounded, size: 30, color: Colors.white),
               label: 'MyBooks',
             ),
             NavigationDestination(
